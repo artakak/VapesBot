e77cd68880696cf44b07c4e34dad25f682a147f2
win7_test <a260641139@ya.ru>
2016-04-13 17:18:08 +0400

    nvbnvbn

--- VapesBot.py
+++ VapesBot.py
@@ -8,6 +8,7 @@ import re
 import requests
 import json
 from sqlalchemy_wrapper import SQLAlchemy
+import random
 
 db = SQLAlchemy('sqlite:///Test.db')
 
@@ -55,8 +56,8 @@ class ChinaBot:
 
     help_text = (
         'Функционал:\n'
-        '/TOP - Подборка товаров по рейтингу\n'
-        '/random - Случайная подборка товаров\n'
+        '/TOP - Подборка товаров с наивысшим рейтингом\n'
+        '/random - Показ случайного товара\n'
         '/search - Поиск товаров по названию\n'
         '/photo - Вывод фотографий для текущего товара\n'
         '/help - Список комманд\n'
@@ -76,11 +77,15 @@ class ChinaBot:
         dp.addTelegramCommandHandler('next', self.get_next)
         dp.addTelegramCommandHandler('help', self.help)
         dp.addTelegramCommandHandler('about', self.about)
-        dp.addTelegramCommandHandler('TOP', self.give)
+        dp.addTelegramCommandHandler('TOP', self.top)
+        dp.addTelegramCommandHandler('sort_up', self.top_up)
+        dp.addTelegramCommandHandler('sort_down', self.top_down)
+        dp.addTelegramCommandHandler('search_sort_up', self.search_up)
+        dp.addTelegramCommandHandler('search_sort_down', self.search_down)
         dp.addTelegramCommandHandler('search', self.search)
         dp.addTelegramCommandHandler('photo', self.photo)
         dp.addTelegramMessageHandler(self.do_search)
-        dp.addTelegramCommandHandler('random', self.give)
+        dp.addTelegramCommandHandler('random', self.random)
 
         dp.addUnknownTelegramCommandHandler(self.unknow)
 
@@ -89,6 +94,7 @@ class ChinaBot:
         self.count = {}
         self.photo = {}
         self.photo_count = {}
+        self.search_query = {}
 
     def logger_wrap(self, message, command):
         if self.botan:
@@ -102,13 +108,27 @@ class ChinaBot:
                                             user.username,
                                             user.last_name))
 
-    def product_wrap(self):
+    def product_wrap(self, bot, update, args):
         try:
-            products = db.query(Product).filter_by(score=5).limit(30).all()
+            if args == 'TOP_Up':
+                products = db.query(Product).filter_by(score=5).order_by(Product.product_price).all()
+            elif args == 'TOP_Down':
+                products = db.query(Product).filter_by(score=5).order_by(Product.product_price.desc()).all()
+            elif args == 'Random':
+                count = db.query(Product).count()
+                products = db.query(Product).filter_by(id=random.randint(1, count)).limit(10).all()
+            elif args == 'Search_Down':
+                string = str(self.search_query[str(update.message.chat_id)])
+                products = db.query(Product).filter(Product.product_name.contains("%"+string+"%")).order_by(Product.product_price.desc()).all()
+            elif args == 'Search_Up':
+                string = str(self.search_query[str(update.message.chat_id)])
+                products = db.query(Product).filter(Product.product_name.contains("%"+string+"%")).order_by(Product.product_price).all()
+
+
         except Exception as e:
             logger.exception(e)
 
-        print(products)
+        #print(products)
         final = [u'*Наименование*: '+product.product_name+'\n'
                  u'*Рейтинг*: '+Emoji.WHITE_MEDIUM_STAR.decode('utf-8')*int(product.score)+'\n'
                  u'*Цена*: '+str(product.product_price)+u' РУБ\n'
@@ -126,7 +146,8 @@ class ChinaBot:
 
     def start(self, bot, update):
         self.logger_wrap(update.message, 'start')
-        custom_keyboard = [['/TOP','/random'],['/search','/help']]
+        custom_keyboard = [['/TOP '+Emoji.WHITE_MEDIUM_STAR.decode('utf-8'),'/random '+Emoji.BLACK_QUESTION_MARK_ORNAMENT.decode('utf-8')],
+                           ['/search '+Emoji.RIGHT_POINTING_MAGNIFYING_GLASS.decode('utf-8'),'/help '+Emoji.ORANGE_BOOK.decode('utf-8')]]
         reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
         bot.sendMessage(update.message.chat_id, text=self.help_text, reply_markup=reply_markup)
 
@@ -144,18 +165,59 @@ class ChinaBot:
 
     def do_search(self, bot, update):
         self.logger_wrap(update.message, 'do_search')
-        self.custom_keyboard = [['/previous','/next'],['/photo','/close']]
-        self.reply_markup = telegram.ReplyKeyboardMarkup(self.custom_keyboard, resize_keyboard=True)
-        bot.sendMessage(update.message.chat_id, text=update.message.text+u' Результат поиска будет тут', parse_mode=ParseMode.MARKDOWN, reply_markup=self.reply_markup)
+        self.search_query[str(update.message.chat_id)] = update.message.text
+        self.give(bot,update, 'Search_Down')
 
-    def give(self, bot, update):
+
+    def give(self, bot, update, args):
         self.logger_wrap(update.message, 'give')
-        self.custom_keyboard = [['/previous','/next'],['/photo','/close']]
+        if args in ['Search_Down','Search_Up']:
+            self.custom_keyboard = [['/previous','/next'],
+                                    ['/search_sort_up '+Emoji.HEAVY_DOLLAR_SIGN.decode('utf-8')+Emoji.UPWARDS_BLACK_ARROW.decode('utf-8'),'/search_sort_down '+Emoji.HEAVY_DOLLAR_SIGN.decode('utf-8')+Emoji.DOWNWARDS_BLACK_ARROW.decode('utf-8')],
+                                    ['/photo','/close']]
+        else:
+            self.custom_keyboard = [['/previous','/next'],
+                                    ['/sort_up '+Emoji.HEAVY_DOLLAR_SIGN.decode('utf-8')+Emoji.UPWARDS_BLACK_ARROW.decode('utf-8'),'/sort_down '+Emoji.HEAVY_DOLLAR_SIGN.decode('utf-8')+Emoji.DOWNWARDS_BLACK_ARROW.decode('utf-8')],
+                                    ['/photo','/close']]
         self.reply_markup = telegram.ReplyKeyboardMarkup(self.custom_keyboard, resize_keyboard=True)
-        self.result[str(update.message.chat_id)] = self.product_wrap()
+        self.result[str(update.message.chat_id)] = self.product_wrap(bot, update, args)
+        self.count[str(update.message.chat_id)] = 0
+        self.photo_count = 0
+        try:
+            bot.sendMessage(update.message.chat_id, text=u'1 ИЗ %s\n' % (str(len(self.result[str(update.message.chat_id)])))+self.result[str(update.message.chat_id)][self.count[str(update.message.chat_id)]], parse_mode=ParseMode.MARKDOWN, reply_markup=self.reply_markup)
+        except:
+            bot.sendMessage(update.message.chat_id, text=u'Извините, мне нечего Вам показать '+Emoji.CONFUSED_FACE.decode('utf-8'), parse_mode=ParseMode.MARKDOWN)
+
+
+    def top(self, bot, update):
+        self.logger_wrap(update.message, 'top')
+        self.give(bot, update, 'TOP_Down')
+
+    def random(self, bot, update):
+        self.logger_wrap(update.message, 'random')
+        self.custom_keyboard = [['/random'],
+                                ['/photo','/close']]
+        self.reply_markup = telegram.ReplyKeyboardMarkup(self.custom_keyboard, resize_keyboard=True)
+        self.result[str(update.message.chat_id)] = self.product_wrap('Random')
         self.count[str(update.message.chat_id)] = 0
         self.photo_count = 0
         bot.sendMessage(update.message.chat_id, text=self.result[str(update.message.chat_id)][self.count[str(update.message.chat_id)]], parse_mode=ParseMode.MARKDOWN, reply_markup=self.reply_markup)
+
+    def top_down(self, bot, update):
+        self.logger_wrap(update.message, 'top_down')
+        self.give(bot, update, 'TOP_Down')
+
+    def top_up(self, bot, update):
+        self.logger_wrap(update.message, 'top_up')
+        self.give(bot, update, 'TOP_Up')
+
+    def search_down(self, bot, update):
+        self.logger_wrap(update.message, 'search_down')
+        self.give(bot, update, 'Search_Down')
+
+    def search_up(self, bot, update):
+        self.logger_wrap(update.message, 'search_up')
+        self.give(bot, update, 'Search_Up')
 
     def photo(self, bot, update):
         self.logger_wrap(update.message, 'photo')
@@ -173,23 +235,27 @@ class ChinaBot:
         self.logger_wrap(update.message, 'next')
         try:
             self.count[str(update.message.chat_id)] += 1
-            bot.sendMessage(update.message.chat_id, text=self.result[str(update.message.chat_id)][self.count[str(update.message.chat_id)]], parse_mode=ParseMode.MARKDOWN, reply_markup=self.reply_markup)
+            bot.sendMessage(update.message.chat_id, text=u'%s ИЗ %s\n' % (str(self.count[str(update.message.chat_id)]+1), str(len(self.result[str(update.message.chat_id)])))+self.result[str(update.message.chat_id)][self.count[str(update.message.chat_id)]], parse_mode=ParseMode.MARKDOWN, reply_markup=self.reply_markup)
         except:
-            self.start(bot, update)
+            bot.sendMessage(update.message.chat_id, text=u'Извините, это последний элемент в данной подборке '+Emoji.CONFUSED_FACE.decode('utf-8'), parse_mode=ParseMode.MARKDOWN)
+            #self.start(bot, update)
 
     def get_previous(self, bot, update):
         self.logger_wrap(update.message, 'previous')
         try:
-            self.count[str(update.message.chat_id)] -= 1
-            bot.sendMessage(update.message.chat_id, text=self.result[str(update.message.chat_id)][self.count[str(update.message.chat_id)]], parse_mode=ParseMode.MARKDOWN, reply_markup=self.reply_markup)
+            if self.count[str(update.message.chat_id)] >= 1:
+                self.count[str(update.message.chat_id)] -= 1
+                bot.sendMessage(update.message.chat_id, text=u'%s ИЗ %s\n' % (str(self.count[str(update.message.chat_id)]+1), str(len(self.result[str(update.message.chat_id)])))+self.result[str(update.message.chat_id)][self.count[str(update.message.chat_id)]], parse_mode=ParseMode.MARKDOWN, reply_markup=self.reply_markup)
         except:
-            self.start(bot, update)
+            bot.sendMessage(update.message.chat_id, text=u'Извините, это первый элемент в данной подборке '+Emoji.CONFUSED_FACE.decode('utf-8'), parse_mode=ParseMode.MARKDOWN)
+            #self.start(bot, update)
 
     def close(self, bot, update):
         self.logger_wrap(update.message, 'close')
         try:
-            self.count[str(update.message.chat_id)] += 0
+            self.count[str(update.message.chat_id)] = 0
             self.result[str(update.message.chat_id)] = []
+            self.search_query[str(update.message.chat_id)] = ''
             self.start(bot, update)
         except:
             self.start(bot, update)
@@ -208,10 +274,10 @@ class ChinaBot:
 
 def main():
     try:
-        #bot_token = '219477880:AAFt3Mug_afgVZdwR-ZWFSMJuUbUBQjM5Mk'
-        #botan_token = 'UGbapiL6McQPN02FfOy9iTdCNOa9l9E9'
-        bot_token = '207682614:AAHfnPbjo4RTAgov8cfEo3erRTLvmx43Ffg'
-        botan_token = 'A6C6UgwxORRchQbmFkqHJl56SmL-G4iy'
+        bot_token = '219477880:AAFt3Mug_afgVZdwR-ZWFSMJuUbUBQjM5Mk'
+        botan_token = 'UGbapiL6McQPN02FfOy9iTdCNOa9l9E9'
+        #bot_token = '207682614:AAHfnPbjo4RTAgov8cfEo3erRTLvmx43Ffg'
+        #botan_token = 'A6C6UgwxORRchQbmFkqHJl56SmL-G4iy'
 
     except Exception as e:
         logger.exception(e)
